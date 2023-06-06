ff13e8c341ef7c305cb5b33ed46c02bd66844da6
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-06-06 16:46:18 +0200

    create "CommonLib_VRML" as reduced copy of "CommonLib"

--- src/net/schwarzbaer/java/lib/image/bumpmapping/Debug.java
+++
@@ -1,7 +0,0 @@
-package net.schwarzbaer.java.lib.image.bumpmapping;
-
-public class Debug {
-	static void Assert(boolean condition) {
-		if (!condition) throw new IllegalStateException();
-	}
-}
--- src/net/schwarzbaer/java/lib/system/ValueContainer.java
+++
@@ -1,9 +0,0 @@
-package net.schwarzbaer.java.lib.system;
-
-public class ValueContainer<Value>
-{
-	public Value value;
-	public ValueContainer(Value value) { this.value = value; }
-	public Value getValue()            { return value; }
-	public void  setValue(Value value) { this.value = value; }
-}
